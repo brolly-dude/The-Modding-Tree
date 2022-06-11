9ff2e2317d8747fdccbed91595c5bb3fdddff7ff
eggyeggyolk <65613457+brolly-dude@users.noreply.github.com>
2022-06-11 15:44:15 +0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -31,7 +31,7 @@ addLayer("d", {
     symbol: "D", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
-        unlocked: false,
+        unlocked: true,
 		prestige: new Decimal(0),
     }},
     color: "#FF0000",

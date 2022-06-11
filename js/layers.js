bd072a7b7ce3b9b352e3e45e356d85a21b1640b4
eggyeggyolk <65613457+brolly-dude@users.noreply.github.com>
2022-06-11 15:45:04 +0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -32,13 +32,13 @@ addLayer("d", {
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
-		prestige: new Decimal(0),
+		points: new Decimal(0),
     }},
     color: "#FF0000",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "Death", // Name of prestige currency
     baseResource: "prestige", // Name of resource prestige is based on
-    baseAmount() {return player.prestige}, // Get the current amount of baseResource
+    baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
